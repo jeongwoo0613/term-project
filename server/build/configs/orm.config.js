a0a086283228005d1cd87a86884d1cb14d39fa42
jeongwoo0613 <some@kakao.com>
2021-08-29 20:14:31 +0900

    fix temporal

--- server/build/configs/orm.config.js
+++ server/build/configs/orm.config.js
@@ -8,7 +8,7 @@ exports.default = {
     username: process.env.TYPEORM_USERNAME,
     password: process.env.TYPEORM_PASSWORD,
     database: process.env.TYPEORM_DATABASE,
-    synchronize: false,
+    synchronize: true,
     logging: false,
     dropSchema: false,
     entities: [root + "/entities/**/*.{js,ts}"],
--- server/src/configs/orm.config.ts
+++ server/src/configs/orm.config.ts
@@ -9,7 +9,7 @@ export default {
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
-  synchronize: false,
+  synchronize: true,
   logging: false,
   dropSchema: false,
   entities: [root + "/entities/**/*.{js,ts}"],
