bf5d4d7a13fc1ab102fbce486d34f6309ea02a1e
jeongwoo0613 <some@kakao.com>
2021-09-04 13:24:26 +0900

    add build schema

--- server/build/schemas/auth.schema.js
+++ server/build/schemas/auth.schema.js
@@ -6,7 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.signupSchema = void 0;
 const joi_1 = __importDefault(require("joi"));
 const signupSchema = joi_1.default.object({
-    userId: joi_1.default.string().alphanum().min(4).max(16).required(),
+    userId: joi_1.default.string()
+        .pattern(/^[a-z0-9]+$/)
+        .min(4)
+        .max(16)
+        .required(),
     password: joi_1.default.string()
         .pattern(/(?=.*[0-9])(?=.*[a-z])(?=.*[^0-9a-zA-Z])/)
         .required(),
