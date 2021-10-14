acd443ac1211793cb2c556c6da833dafffa91639
jeongwoo0613 <some@kakao.com>
2021-10-14 19:57:07 +0900

    fix path

--- server/@types/express/index.d.ts
+++ server/@types/express/index.d.ts
@@ -4,10 +4,10 @@ declare namespace Express {
       location: string;
       key: string;
     };
-    user: import("../../src/entities/user.entity").User;
-    userByUserId: import("../../src/entities/user.entity").User;
-    post: import("../../src/entities/post.entity").Post;
-    coin: import("../../src/entities/coin.entity").Coin;
-    comment: import("../../src/entities/comment.entity").Comment;
+    user: import("../../src/entities").User;
+    userByUserId: import("../../src/entities").User;
+    post: import("../../src/entities").Post;
+    coin: import("../../src/entities").Coin;
+    comment: import("../../src/entities").Comment;
   }
 }
